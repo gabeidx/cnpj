f557d6c401563d47c603103594f37be702eb60c1
Gabriel Izaias <gabriel.izaias@gmail.com>
2020-06-21 09:44:53 +0200

    update prettier config

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const digit = numbers => {
-  let index = 2
+const digit = (numbers) => {
+	let index = 2
 
-  const reverse = numbers
-    .split('')
-    .reduce((buffer, number) => [+number].concat(buffer), [])
+	const reverse = numbers.split('').reduce((buffer, number) => [+number].concat(buffer), [])
 
-  const sum = reverse.reduce((buffer, number) => {
-    buffer += number * index
-    index = index === 9 ? 2 : index + 1
-    return buffer
-  }, 0)
+	const sum = reverse.reduce((buffer, number) => {
+		buffer += number * index
+		index = index === 9 ? 2 : index + 1
+		return buffer
+	}, 0)
 
-  const mod = sum % 11
+	const mod = sum % 11
 
-  return mod < 2 ? 0 : 11 - mod
+	return mod < 2 ? 0 : 11 - mod
 }
 
 /**
@@ -22,25 +20,25 @@ const digit = numbers => {
  * @return {Boolean}
  */
 export function validate(cnpj) {
-  // Remove period, slash and dash characters from CNPJ
-  const cleaned = cnpj.toString().replace(/[\.\/\-]/g, '')
+	// Remove period, slash and dash characters from CNPJ
+	const cleaned = cnpj.toString().replace(/[\.\/\-]/g, '')
 
-  if (
-    // Must be defined
-    !cleaned ||
-    // Must have 14 characters
-    cleaned.length !== 14 ||
-    // Must be digits and not be sequential characters (e.g.: 11111111111111, etc)
-    /^(\d)\1+$/.test(cleaned)
-  ) {
-    return false
-  }
+	if (
+		// Must be defined
+		!cleaned ||
+		// Must have 14 characters
+		cleaned.length !== 14 ||
+		// Must be digits and not be sequential characters (e.g.: 11111111111111, etc)
+		/^(\d)\1+$/.test(cleaned)
+	) {
+		return false
+	}
 
-  let registration = cleaned.substr(0, 12)
-  registration += digit(registration)
-  registration += digit(registration)
+	let registration = cleaned.substr(0, 12)
+	registration += digit(registration)
+	registration += digit(registration)
 
-  return registration.substr(-2) === cleaned.substr(-2)
+	return registration.substr(-2) === cleaned.substr(-2)
 }
 
 /**
@@ -49,15 +47,14 @@ export function validate(cnpj) {
  * @return {String} The formatted CNPJ
  */
 export function format(cnpj) {
-  return cnpj
-    .toString()
-    // Remove non digit characters
-    .replace(/[^\d]/g, '')
-    // Apply formatting
-    .replace(
-      /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/,
-      '$1.$2.$3/$4-$5'
-    )
+	return (
+		cnpj
+			.toString()
+			// Remove non digit characters
+			.replace(/[^\d]/g, '')
+			// Apply formatting
+			.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5')
+	)
 }
 
 /**
@@ -65,15 +62,15 @@ export function format(cnpj) {
  * @return {String} The generated CNPJ
  */
 export function generate() {
-  let cnpj = ''
-  let i = 12
+	let cnpj = ''
+	let i = 12
 
-  while (i--) {
-    cnpj += Math.floor(Math.random() * 9)
-  }
+	while (i--) {
+		cnpj += Math.floor(Math.random() * 9)
+	}
 
-  cnpj += digit(cnpj)
-  cnpj += digit(cnpj)
+	cnpj += digit(cnpj)
+	cnpj += digit(cnpj)
 
-  return format(cnpj)
+	return format(cnpj)
 }
