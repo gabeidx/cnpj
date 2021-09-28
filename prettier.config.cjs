8beedbcbdde425c9ab2523e2668a2a9e52028c8a
Gabriel Silva <gabriel.izaias@gmail.com>
2021-09-28 10:53:37 +0200

    update prettier configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	parser: 'typescript',
+	printWidth: 120,
+	quoteProps: 'consistent',
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+}
